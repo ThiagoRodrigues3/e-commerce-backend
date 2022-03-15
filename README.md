de9f35f2f7cf86a3971e30398e4bebd1177cc0cc
Thiago Rod <whytigo@gmail.com>
2022-03-15 04:21:51 -0400

    readme file and some fixes

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# e-commerce-backend
-The back-end database api needed for an e-commerce website.
+# E-commerce Back End
+
+### Descritiption
+Development of the back end for an e-commerce site using Express.js API, configured to use sequelize to interact with a MySQL database. 
+
+### Built With 🧰
+- [x] Express.js
+- [x] MySQL
+- [x] Sequelize
+
+### How to use
+To get started:
+
+` Download the code or clone the repo`                    
+` npm i to install necessary packages `       
+` npm run seed to populate database `       
+` npm start` 
+
+## Project Visual 😎
+### See Video Demo 
+The video is [here](https://drive.google.com/file/d/1dtFAjLMk20hUMDCEsu7UtjB7j1C2T6_c/view?usp=sharing)
+
+
+## Acknowledgement, Documentation & Resources 🤝
+- [Sequelize Documentation](https://sequelize.org/v5/)
+- [Workbench documentation](https://dev.mysql.com/doc/workbench/en/)
+- [MySQL2 Documentation](https://www.npmjs.com/package/mysql2)
+- [SQL Troubleshooting Resources on Stack Overflow](https://stackoverflow.com/questions/26554818/using-mysql-in-the-command-line-in-osx-command-not-found)
+- [SQL Deferrable Constraints Resoruces](https://begriffs.com/posts/2017-08-27-deferrable-sql-constraints.html)
+- [All you need to know about Async Await In JavaScript](https://medium.com/technofunnel/javascript-async-await-c83b15950a71)
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
       include: [{model: Product}],
@@ -16,9 +12,7 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{model: Product}],
@@ -35,8 +29,7 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
@@ -45,8 +38,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
   try {
     const categoryData = await Category.update(req.body, {
       where: {
@@ -65,8 +57,7 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
   try {
     const categoryData = await Category.destroy({
       where: {
